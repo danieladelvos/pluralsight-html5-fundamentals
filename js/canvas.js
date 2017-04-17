55d8d0c8c98e699214330c41a1cc847f1d57ab90
Daniela Delvos <daniela.delvos@gmail.com>
2017-04-17 08:44:16 +0200

    canvas basic shape

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-  console.log('testcanvas');
+  var canvas, context;
+  canvas = document.getElementById('canvas');
+
+  if (canvas && canvas.getContext) {
+    context = canvas.getContext('2d');
+  context.beginPath();
+  context.moveTo(75, 50);
+  context.lineTo(75, 100);
+  context.lineTo(25, 100);
+  context.fill();
+  }
 });
