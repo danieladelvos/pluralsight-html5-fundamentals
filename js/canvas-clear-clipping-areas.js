488c2aeba5420b720a49841e9a4a61a9b49d12ce
Daniela Delvos <daniela.delvos@gmail.com>
2017-04-23 10:59:07 +0200

    canvas clipping non-contiguous areas

--- js/canvas-clear-clipping-areas.js
+++ js/canvas-clear-clipping-areas.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function(e) {
   var context = canvas.getContext('2d');
   var width = canvas.width, height = canvas.height;
 
-  context.fillStyle = 'rgba(153, 153, 153, 0.75)';
-  context.strokeStyle = '#999999';
+  context.fillStyle = 'rgba(0, 200, 200, 0.75)';
+  context.strokeStyle = '#999900';
   context.lineWidth = 5;
   context.lineCap = 'round';
 
@@ -28,6 +28,22 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
   context.restore();
 
+  context.save();
+
   context.fillRect(200, 200, 50, 50);
+
+  context.rect(0, 350, 100, 100);
+  context.rect(105, 350, 100, 100);
+
+  context.clip();
+
+  context.stroke();
+
+  context.fillRect(0, 350, 300, 300);
+
+  context.restore();
+
+  context.fillRect(0, 455, 100, 100);
+
 
 });
