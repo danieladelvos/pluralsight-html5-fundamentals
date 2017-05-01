2b48693c0a218d5ab69b8d97c13a5074610fefba
Daniela Delvos <daniela.delvos@gmail.com>
2017-05-01 15:29:56 +0200

    chart static

--- js/chart.js
+++ js/chart.js
@@ -4,6 +4,12 @@ document.addEventListener('DOMContentLoaded', function(e) {
   var img = new Image();
   img.onload = function() {
     context.drawImage(img, 0, 0);
+    context.beginPath();
+    context.moveTo(70, 105);
+    context.lineTo(105, 132);
+    context.lineTo(142, 250);
+    context.lineTo(130, 115);
+    context.stroke();
   };
 
   img.src = "img/chart-background-large.png";
