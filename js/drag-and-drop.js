218e72f37daf4e718642649fcc07095db0514cbd
Daniela Delvos <daniela.delvos@gmail.com>
2017-05-07 10:29:42 +0200

    drag and drop role selectors

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(e) {
+  var sourceContainerId;
   var dragStart = function(e) {
     // IE does not support text/plain
     try {
@@ -6,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
     } catch (ex) {
       e.dataTransfer.setData('Text', e.target.id);
     }
+    sourceContainerId = this.parentElement.id;
   };
   var cancel = function(e) {
     if (e.preventDefault) {
@@ -18,20 +20,24 @@ document.addEventListener('DOMContentLoaded', function(e) {
   };
   var dropped = function(e) {
     var id;
-    cancel(e);
-    try {
-      id = e.dataTransfer.getData('text/plain');
-    } catch (ex) {
-      id = e.dataTransfer.detData('Text');
+    if (this.id !== sourceContainerId) {
+      cancel(e);
+      try {
+        id = e.dataTransfer.getData('text/plain');
+      } catch (ex) {
+        id = e.dataTransfer.detData('Text');
+      }
+      e.target.appendChild(document.querySelector('#' + id));
     }
-    e.target.appendChild(document.querySelector('#' + id));
   };
-
-  var img = document.querySelector('#puppies-01');
-  img.addEventListener('dragstart', dragStart, false);
-
-  var target = document.querySelector('#target-container');
-  target.addEventListener('drop', dropped, false);
-  target.addEventListener('dragenter', cancel, false);
-  target.addEventListener('dragover', cancel, false);
+  var targets = document.querySelectorAll('[data-role="drag-drop-target"]');
+  [].forEach.call(targets, function(target) {
+    target.addEventListener('drop', dropped, false);
+    target.addEventListener('dragenter', cancel, false);
+    target.addEventListener('dragover', cancel, false);
+  });
+  var sources = document.querySelectorAll('[draggable="true"]');
+  [].forEach.call(sources, function(source) {
+    source.addEventListener('dragstart',dragStart, false);
+  })
 });
