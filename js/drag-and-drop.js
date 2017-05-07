4d4f97ab28955fdb6cdfb980d0467266c1492227
Daniela Delvos <daniela.delvos@gmail.com>
2017-05-07 10:01:57 +0200

    drag and drop basics

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -1,12 +1,30 @@
 document.addEventListener('DOMContentLoaded', function(e) {
   var dragStart = function(e) {
-
+    // IE does not support text/plain
+    try {
+      e.dataTransfer.setData('text/plain', e.target.id);
+    } catch (ex) {
+      e.dataTransfer.setData('Text', e.target.id);
+    }
   };
   var cancel = function(e) {
-    e.preventDefault();
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    }
+    return false;
   };
   var dropped = function(e) {
-    console.log(e);
+    var id;
+    cancel(e);
+    try {
+      id = e.dataTransfer.getData('text/plain');
+    } catch (ex) {
+      id = e.dataTransfer.detData('Text');
+    }
+    e.target.appendChild(document.querySelector('#' + id));
   };
 
   var img = document.querySelector('#puppies-01');
