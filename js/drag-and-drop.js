9cc560b52a7e4477d3c93104411fff7b345fc2fc
Daniela Delvos <daniela.delvos@gmail.com>
2017-05-07 10:55:35 +0200

    drag and drop events

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(e) {
   var sourceContainerId;
   var dragStart = function(e) {
+    console.log('source: dragstart');
     // IE does not support text/plain
     try {
       e.dataTransfer.setData('text/plain', e.target.id);
     } catch (ex) {
       e.dataTransfer.setData('Text', e.target.id);
     }
+    e.dataTransfer.effectAllowed = 'move';
     sourceContainerId = this.parentElement.id;
   };
   var cancel = function(e) {
@@ -17,6 +19,12 @@ document.addEventListener('DOMContentLoaded', function(e) {
       e.stopPropagation();
     }
     return false;
+  };
+  var dragEnd = function(e) {
+    console.log('source: dragend');
+  };
+  var drag = function(e) {
+    console.log('source: drag');
   };
   var dropped = function(e) {
     var id;
@@ -30,14 +38,28 @@ document.addEventListener('DOMContentLoaded', function(e) {
       e.target.appendChild(document.querySelector('#' + id));
     }
   };
+  var dragEnter = function(e) {
+    cancel(e);
+    console.log('target: dragenter');
+  };
+  var dragOver = function (e) {
+    cancel(e);
+    console.log('target: dragover');
+  };
+  var dragLeave = function (e) {
+    console.log('target: dragleave');
+  };
   var targets = document.querySelectorAll('[data-role="drag-drop-target"]');
   [].forEach.call(targets, function(target) {
     target.addEventListener('drop', dropped, false);
     target.addEventListener('dragenter', cancel, false);
     target.addEventListener('dragover', cancel, false);
+    target.addEventListener('dragleave', dragLeave, false);
   });
   var sources = document.querySelectorAll('[draggable="true"]');
   [].forEach.call(sources, function(source) {
     source.addEventListener('dragstart',dragStart, false);
+    source.addEventListener('drag', drag, false);
+    source.addEventListener('dragend', dragEnd, false);
   })
 });
